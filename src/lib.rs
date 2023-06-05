1b91ce87cbb214f0eaf70e183ca54efb9cd552e1
jknoptrix_ <quownxhutw@gmail.com>
2023-06-05 12:12:05 +0500

    new macros, log level, better logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,94 @@
-use colored::Colorize;
 use chrono::Local;
+use std::fs::OpenOptions;
+use std::io::{self, Write};
+use std::path::Path;
+use std::sync::Mutex;
+
+lazy_static::lazy_static! {
+    static ref LOG_LEVEL: Mutex<LogLevel> = Mutex::new(LogLevel::Console);
+}
+
+#[derive(Copy, Clone)]
+pub enum LogLevel {
+    Console,
+    File,
+    Both,
+}
+
+pub fn set_log_level(level: LogLevel) {
+    let mut log_level = LOG_LEVEL.lock().unwrap();
+    *log_level = level;
+}
+
+fn log_to_file(now: &str, message: &str) -> io::Result<()> {
+    let filename = format!("{}.log", Local::now().format("%Y-%m-%d"));
+    let path = Path::new(&filename);
+    let mut file = OpenOptions::new().create(true).append(true).open(path)?;
+    writeln!(file, "{} {}", now, message)?;
+    Ok(())
+}
 
 pub fn error(now: &str, message: &str) {
-    // define error function for printing error messages
-    eprintln!("{} {} {}", now.bold().dimmed(), "[ERROR]".bold().red(), message.red()); // print formatted error message to stderr
+    let message = format!("\x1b[2m\x1b[1m{}\x1b[0m \x1b[31m\x1b[1m[ERROR]\x1b[0m \x1b[31m{}\x1b[0m", now, message);
+    let log_level = LOG_LEVEL.lock().unwrap();
+    match *log_level {
+        LogLevel::Console => eprintln!("{}", message),
+        LogLevel::File => {
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+        LogLevel::Both => {
+            eprintln!("{}", message);
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+    }
 }
 
 pub fn warn(now: &str, message: &str) {
     // define warn function for printing warning messages
-    eprintln!("{} {} {}", now.bold().dimmed(), "[WARN]".bold().yellow(), message.yellow()); // print formatted warning message to stderr
+    eprintln!("\x1b[2m\x1b[1m{}\x1b[0m \x1b[33m\x1b[1m[WARN]\x1b[0m \x1b[33m{}\x1b[0m", now, message); // print formatted warning message to stderr
+    let log_level = LOG_LEVEL.lock().unwrap();
+    match *log_level {
+        LogLevel::Console => eprintln!("{}", message),
+        LogLevel::File => {
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+        LogLevel::Both => {
+            eprintln!("{}", message);
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+    }
 }
 
 pub fn info(now: &str, message: &str) {
     // define info function for printing info messages
-    println!("{} {} {}", now.bold().dimmed(), "[INFO]".bold().cyan(), message.cyan()); // print formatted info message to stdout
+    println!("\x1b[2m\x1b[1m{}\x1b[0m \x1b[36m\x1b[1m[INFO]\x1b[0m \x1b[36m{}\x1b[0m", now, message); // print formatted info message to stdout
+    let log_level = LOG_LEVEL.lock().unwrap();
+    match *log_level {
+        LogLevel::Console => eprintln!("{}", message),
+        LogLevel::File => {
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+        LogLevel::Both => {
+            eprintln!("{}", message);
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+    }
+}
+
+pub fn debug(now: &str, message: &str) {
+    // define debug function for printing debug messages
+    println!("\x1b[2m\x1b[1m{}\x1b[0m \x1b[34m\x1b[1m[DEBUG]\x1b[0m \x1b[34m{}\x1b[0m", now, message); // print formatted debug message to stdout
+    let log_level = LOG_LEVEL.lock().unwrap();
+    match *log_level {
+        LogLevel::Console => eprintln!("{}", message),
+        LogLevel::File => {
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+        LogLevel::Both => {
+            eprintln!("{}", message);
+            log_to_file(now, &message).unwrap_or_else(|e| eprintln!("Failed to write to log file: {}", e));
+        }
+    }
 }
 
 pub fn current_time() -> String {
@@ -24,8 +99,8 @@ pub fn current_time() -> String {
 macro_rules! log_error {
     // define log_error macro for logging error messages
     ($($arg:tt)*) => {{
-        let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        error(&now, &format!($($arg)*)); // call error function with formatted arguments
+        let now = $crate::current_time();
+        $crate::error(&now, &format!($($arg)*));
     }}
 }
 
@@ -33,8 +108,8 @@ macro_rules! log_error {
 macro_rules! log_warn {
     // define log_warn macro for logging warning messages
     ($($arg:tt)*) => {{
-        let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        warn(&now, &format!($($arg)*)); // call warn function with formatted arguments
+        let now = $crate::current_time();
+        $crate::warn(&now, &format!($($arg)*)); // call warn function with formatted arguments
     }}
 }
 
@@ -42,8 +117,16 @@ macro_rules! log_warn {
 macro_rules! log_info {
     // define log_info macro for logging info messages
     ($($arg:tt)*) => {{
-        let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        info(&now, &format!($($arg)*)); // call info function with formatted arguments
+        let now = $crate::current_time();
+        $crate::info(&now, &format!($($arg)*)); // call info function with formatted arguments
     }}
 }
 
+#[macro_export]
+macro_rules! log_debug {
+    // define log_debug macro for logging info messages
+    ($($arg:tt)*) => {{
+        let now = $crate::current_time();
+        $crate::debug(&now, &format!($($arg)*)); // call info function with formatted arguments
+    }}
+}
